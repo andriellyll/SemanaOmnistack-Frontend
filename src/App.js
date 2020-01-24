ecba4e43e6669d009a6692fba5b63a5f3f2bc814
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2020-01-24 12:30:57 -0300

    web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from 'react';
+import api from './services/api';
+// Componente => (bloco isolado) função que retorna um código html, css, ou javascript(quando se trata de dinamicidade na página)
+//    um componente não influencia no outro. Cada componente eh separado em seu arquivo
+// Propridade => atributo do componente (title, src, etc). Todas as propriedades devem ser passadas como parâmetro atraves do props.
+//    A propriedade, dentro da tag, deve ser escrita entre chaves, para o seu valor ser visivel
+// Para se usar varios componentes seguidos, eles devem estar dentro de um container => usa-se a fragment (tag sem nomenclatura) => <> </>
+// Outra definição de propriedade: Informações que o componente PAI passa para o componente FILHO
+// Estado => Informação que o componente vai manipular, manter e atualizar 
+
+// const [counter, setCounter] = useState(0); 0 eh o valor inicial da variavel
+// function incrementCounter(){
+//    setCounter(counter + 1);
+// }
+
+// conceito Imutabilidade React => nunca sera ALTERADA um dado, sera sempre CRIADO outro dado a partir do primeiro.
+//      operações como contador++, ou alteração de uma variavel nao sao possiveis.
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [devs, setDevs] = useState([]);
+  
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data){
+    
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
+  return(
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev}/>
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (            
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>  
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+// a importação de React from 'react' eh feita para possibilitar isso <App /> => JSX (JavaScript + XML) XML => sintaxe do html
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
